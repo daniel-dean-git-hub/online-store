a157cbf63204385d184c3658cef8e3796f57492f
DanDean191 <Daniel.Dean191@hotmail.com>
2022-05-01 16:39:54 +0100

    Fresh install

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-
+import 'semantic-ui-css/semantic.min.css'
 const container = document.getElementById('root');
 const root = createRoot(container);
 

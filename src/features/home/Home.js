1d2e4aba7301542a1e3ee8174044e71063486955
DanDean191 <Daniel.Dean191@hotmail.com>
2022-05-05 21:12:26 +0100

    tidying up code

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,21 +1,18 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
-import { Grid, Image, Button, Placeholder } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 
 import Banner from './Banner'
 import CategoryButton from './CategoryButton'
 
 import bannerImg from '../../assets/banner.jpg'
-
 import mens from '../../assets/mens.jpg'
 import womens from '../../assets/womens.jpg'
 import jewelery from '../../assets/jewelery.jpg'
 import electronics from '../../assets/electronics.jpg'
 
 const Home = () => {
-
-
   return (
     <Grid stretched centered>
       <Grid.Row as={Link} to={`/Products`}>
@@ -24,7 +21,6 @@ const Home = () => {
       <Grid.Row>
         <Grid.Column width={8} color={'black'}>
           <CategoryButton categoryImg={womens} url={`/Products/women's%20clothing`} text={`Women's`} />
-
         </Grid.Column>
         <Grid.Column width={8} color={'black'}>
           <CategoryButton categoryImg={mens} url={`/Products/men's%20clothing`} text={`Men's`} />
